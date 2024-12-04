0fc3595127ac8ebf0d3fedb15ca13dfc9c7ed5aa
EdwinEngelbrethsen <edwin_engelbrethsen@outlook.com>
2024-12-04 19:45:15 +0100

    day 4 part 2 solution

---
+++ src/main/kotlin/DayFour.kt
@@ -0,0 +1,48 @@
+package org.example
+
+import java.io.File
+
+fun main() {
+    val fileName = "src/main/resources/dayFourInput.txt"
+    val file = File(fileName)
+    val grid = wordSearchHelper(file)
+    val count = countWordOccurences(grid, "XMAS")
+
+    println(count)
+}
+
+fun wordSearchHelper(input: File): List<List<Char>> {
+    val grid = mutableListOf<List<Char>>()
+    input.forEachLine { line ->
+        if(line.isNotBlank()) {
+            val row = line.trim().toCharArray().toList()
+            grid.add(row)
+        }
+    }
+    return grid
+}
+
+fun countWordOccurences(grid: List<List<Char>>, word: String): Int {
+    if(grid.isEmpty() || grid[0].isEmpty()) return 0
+    val numRows = grid.size
+    val numCols = grid[0].size
+    var count = 0
+
+    val validSequences = listOf("MAS", "SAM")
+
+
+    for (row in 1 until numRows - 1) {
+        for (col in 1 until numCols - 1) {
+            val centerChar = grid[row][col].uppercaseChar()
+            if(centerChar != 'A') continue
+
+            val diag1 = "" + grid[row - 1][col - 1] + centerChar + grid[row + 1][col + 1]
+            val diag2 = "" + grid[row -1][col +1 ] + centerChar + grid[row + 1][col - 1]
+
+            if(validSequences.contains(diag1) && validSequences.contains(diag2)) {
+                count++
+            }
+        }
+    }
+    return count
+}
